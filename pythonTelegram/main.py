0beb87b70db33d85af0bee366333b9ebed42532e
Abdulrahmantommy <46884018+Abdulrahmantommy@users.noreply.github.com>
2021-06-01 17:07:48 +0200

    Update main.py

--- pythonTelegram/main.py
+++ pythonTelegram/main.py
@@ -1,35 +1,46 @@
-# pip install requests
-#
-# https://api.telegram.org/bot<token>/getUpdates
-#
-# https://github.com/python-telegram-bot/python-telegram-bot
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
 
+class VideoPlayer(QWidget):
+    def __init__(self, parent=None):
+        super(VideoPlayer, self).__init__(parent)
+        videoItem = QGraphicsVideoItem()
+        videoItem.setSize(QSizeF(640, 480))
+        scene = QGraphicsScene(self)
+        scene.addItem(videoItem)
+        graphicsView = QGraphicsView(scene)
+        layout = QVBoxLayout()
+        layout.addWidget(graphicsView)
+        self.setLayout(layout)
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.mediaPlayer.setVideoOutput(videoItem)
 
+    def keyPressEvent(self, e):
+        print('state: ' + str(self.mediaPlayer.state()))
+        print('mediaStatus: ' + str(self.mediaPlayer.mediaStatus()))
+        print('error: ' + str(self.mediaPlayer.error()))
+        if e.key() == Qt.Key_L:
+            print('loading')
+            self.load()
+        if e.key() == Qt.Key_P:
+            print('playing')
+            self.mediaPlayer.play()
 
+    def load(self):
+        local = QUrl.fromLocalFile('C:\\Users\\tank.sui\\Videos\\Wildlife.wmv')
+        media = QMediaContent(local)
+        self.mediaPlayer.setMedia(media)
 
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    player = VideoPlayer()
+    player.show()
+    sys.exit(app.exec_())
 
+After Review your code， just list the root cause below.QUrl.fromLocalFile
 
-
-
-
-
-
-
-
-
-
-
-import requests
-
-from pprint import pprint
-
-url = "https://api.telegram.org/bot<token>/getUpdates"
-
-response = requests.get(url)
-
-# pprint(response.json())
-
-
-# for msg in response.json()['result'] :
-#     pprint(msg['message']['text'])
-
+local = QUrl.fromLocalFile('')
